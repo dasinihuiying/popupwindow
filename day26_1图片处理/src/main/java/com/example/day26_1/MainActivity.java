2b2c064225ae11e862ff07ba1831b76a2a4bd9f7
lin <123>
2016-10-13 11:48:55 +0800

    第三次上传

--- day26_1图片处理/src/main/java/com/example/day26_1/MainActivity.java
+++ day26_1图片处理/src/main/java/com/example/day26_1/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -43,8 +44,7 @@ public class MainActivity extends AppCompatActivity {
             case R.id.btn_cut:
                 iv.setImageBitmap(util.getCutBitmap(source,0,0,source.getWidth()/2,source.getHeight()/3));
                 break;
-
-
         }
+        Log.i("mTag", "click: ");
     }
 }
